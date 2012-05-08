7257786197b4c821b2ec63bf4efe479ca099ad81
Kasper Johansen <k@spernj.org>
2012-05-08 13:07:17 +0200

    Added documentation for the PHP-script.

--- lib/php_script.php
+++ lib/php_script.php
@@ -1,7 +1,9 @@
 #!/usr/bin/env php5
 <?php
 
+//Controls the PHP-process on the PHP-side.
 class php_process{
+  //Opens stdin and stdout for processing. Sets various helper-variables.
   function __construct(){
     $this->sock_stdin = fopen("php://stdin", "r");
     $this->sock_stdout = fopen("php://stdout", "w");
@@ -16,6 +18,7 @@ class php_process{
     print "php_script_ready:" . getmypid() . "\n";
   }
   
+  //Starts listening in stdin for new instructions. Calls 'handle_line' for every line gotten.
   function start_listening(){
     while(true){
       $line = fgets($this->sock_stdin, 1048576);
@@ -23,6 +26,7 @@ class php_process{
     }
   }
   
+  //Writes the given data to stdout. Serializes and encodes it as well and increases the 'send_count'-variable.
   function send($data){
     $id = $this->send_count;
     $this->send_count++;
@@ -34,6 +38,7 @@ class php_process{
     //return $this->read_answer($id);
   }
   
+  //Handles the given instruction. It parses it and then calls the relevant method.
   function handle_line($line){
     $data = explode(":", $line);
     $type = $data[0];
@@ -63,6 +68,7 @@ class php_process{
     }
   }
   
+  //Parses objects into special arrays, which again will be turned into proxy-objects on the Ruby-side. Recursivly scans arrays to do the same.
   function parse_data($data){
     if (is_array($data)){
       foreach($data as $key => $val){
@@ -96,6 +102,7 @@ class php_process{
     }
   }
   
+  //Recursivly read the given data from the Ruby-side. Changing special arrays into the objects they refer to.
   function read_parsed_data($data){
     if (is_array($data) and array_key_exists("type", $data) and $data["type"] == "proxyobj" and array_key_exists("id", $data)){
       $object = $this->objects[$data["id"]]["obj"];
@@ -118,12 +125,14 @@ class php_process{
     }
   }
   
+  //Answers a request ID with the given data. Writes it to stdout.
   function answer($id, $data){
     if (!fwrite($this->sock_stdout, "answer:" . $id . ":" . base64_encode(serialize($this->parse_data($data))) . "\n")){
       throw new exception("Could not write to socket.");
     }
   }
   
+  //Ruby wants spawn an object. Cache it and return (Ruby will tell us when to unset it automatically).
   function new_object($id, $args){
     $class = $args["class"];
     $new_args = $this->read_parsed_data($args["args"]);
@@ -134,6 +143,7 @@ class php_process{
     $this->answer($id, $object);
   }
   
+  //Ruby wants to set an instance variable on an object. Do that and answer with 'true'.
   function set_var($id, $args){
     $object = $this->objects[$args["id"]]["obj"];
     if (!$object){
@@ -144,6 +154,7 @@ class php_process{
     $this->answer($id, true);
   }
   
+  //Ruby wants to read an instance variable on an object. Return the variable's value.
   function get_var($id, $args){
     $object = $this->objects[$args["id"]]["obj"];
     if (!$object){
@@ -153,6 +164,7 @@ class php_process{
     $this->answer($id, $object->$args["name"]);
   }
   
+  //Ruby wants to call a method on an object. Do that and return the result.
   function object_call($id, $args){
     if (!array_key_exists($args["id"], $this->objects)){
       throw new exception("No object by that ID: " . $args["id"]);
@@ -174,9 +186,9 @@ class php_process{
     $this->answer($id, $res);
   }
   
+  //Ruby wants to call a function. Do that and return the result.
   function func($id, $args){
     //These functions cant be called normally. Hack them with eval instead.
-    
     $newargs = $this->read_parsed_data($args["args"]);
     
     if (in_array($args["func_name"], $this->func_specials)){
@@ -204,6 +216,7 @@ class php_process{
     $this->answer($id, $res);
   }
   
+  //Ruby has given us various object-IDs to unset. Unset them from cache and return 'true'.
   function unset_ids($id, $args){
     foreach($args["ids"] as $obj_id){
       if (!array_key_exists($obj_id, $this->objects)){
@@ -223,6 +236,7 @@ class php_process{
     $this->answer($id, true);
   }
   
+  //Ruby wants information about the object-cache on the PHP-side. Return that in an array.
   function object_cache_info($id, $args){
     $types = array();
     foreach($this->objects as $key => $val){
@@ -239,6 +253,7 @@ class php_process{
     ));
   }
   
+  //Ruby wants to call a static method. Answers with the result.
   function static_method_call($id, $args){
     $call_arr = array($args["class_name"], $args["method_name"]);
     
@@ -256,6 +271,7 @@ class php_process{
     $this->answer($id, $res);
   }
   
+  //Creates a function which can be used for callbacks on the Ruby-side.
   function create_func($id, $args){
     $cb_id = $args["callback_id"];
     $func = create_function("", "global \$php_process; \$php_process->call_back_created_func(" . $cb_id . ", func_get_args());");
@@ -267,6 +283,7 @@ class php_process{
     $this->answer($id, true);
   }
   
+  //This function is called, when a create-function is called. It then callbacks to Ruby, where a 'Proc' will be executed.
   function call_back_created_func($func_id, $args){
     $this->send(array(
       "type" => "call_back_created_func",
@@ -275,6 +292,7 @@ class php_process{
     ));
   }
   
+  //Ruby wants to call a created function. Pretty much just executes that function. This is useually done for debugging callbacks.
   function call_created_func($id, $args){
     $func = $this->created_functions[$args["id"]]["func"];
     if (!$func){
@@ -298,10 +316,12 @@ class php_process{
     $this->answer($id, $res);
   }
   
+  //Ruby wants to read a constant. This is not done by 'func', because of keeping caching posibility open and not wanting to eval.
   function constant_val($id, $args){
     $this->answer($id, constant($args["name"]));
   }
   
+  //Returns various information about the object-cache.
   function memory_info($id, $args){
     $this->answer($id, array(
       "objects" => count($this->objects),
